72689414541f192735240f679d8451bbc2ebc797
Ivan Lebedev <ivan.lebedev@simbirsoft.com>
2021-09-12 17:59:08 +0400

    fix

--- src/_mocks_/account.ts
+++ src/_mocks_/account.ts
@@ -1,8 +1,8 @@
 const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
-  role: 'test'
+  displayName: "Игорь Петров",
+  email: "admin@simbirsoft.cc",
+  photoURL: "/static/mock-images/avatars/avatar_default.jpg",
+  role: "test",
 };
 
 export default account;
